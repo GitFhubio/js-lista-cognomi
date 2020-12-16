64640cb15ecafa1586d14c49b8823060d644f793
Fabio Petrone <fa610movic90@gmail.com>
2020-12-16 13:53:25 +0100

    base

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente [ul, li]
+// solo dopo, scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+var cognome=prompt('Ciao utente, quale è il tuo cognome?');
+var cognomi = ['Bianchi','Rossi','Duzioni','Balsano','Verdi'];
+
+
+var length= cognomi.push(cognome);
+cognomi=cognomi.sort();
+
+var lista= document.getElementById('lista');
+
+for (var i = 0; i < length; i++) {
+ lista.innerHTML+= '<li>'+ cognomi[i] + '</li>';
+}
+
+var indice = cognomi.indexOf(cognome) + 1 ;
+lista.append('La posizione umana in cui si trova '+ cognome + ' è: ' + indice);
