699e7e2ec7a578a1e1a2e6c91729a7986e49c66c
Fabio Petrone <fa610movic90@gmail.com>
2020-12-16 14:51:31 +0100

    versione migliore

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-//
+
 // Chiedere all’utente il cognome
 // inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
 // stampa la lista ordinata alfabeticamente [ul, li]
 // solo dopo, scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
-var cognome=prompt('Ciao utente, quale è il tuo cognome?');
+var input=prompt('Ciao utente, quale è il tuo cognome?');
 var cognomi = ['Bianchi','Rossi','Duzioni','Balsano','Verdi'];
-if(isNaN(cognome) && cognome[0] === cognome[0].toUpperCase()){
-
+if(isNaN(input)){
+var cognome = input.charAt(0).toUpperCase() + input.slice(1);
+// console.log(cognome);
 var length= cognomi.push(cognome);
 cognomi=cognomi.sort();
 
@@ -21,5 +22,30 @@ var indice = cognomi.indexOf(cognome) + 1 ;
 lista.append('La posizione in cui si trova '+ cognome + ' è: ' + indice);
 }
 else{
-  alert('Devi inserire un cognome,e deve iniziare con la lettera maiuscola');
+  alert('Devi inserire un cognome');
 }
+
+
+// versione base
+
+// var cognome=prompt('Ciao utente, quale è il tuo cognome?');
+// var cognomi = ['Bianchi','Rossi','Duzioni','Balsano','Verdi'];
+// if(isNaN(cognome) && cognome[0] === cognome[0].toUpperCase()){
+//
+// var length= cognomi.push(cognome);
+// cognomi=cognomi.sort();
+//
+// var lista= document.getElementById('lista');
+//
+// for (var i = 0; i < length; i++) {
+//  lista.innerHTML+= '<li>'+ cognomi[i] + '</li>';
+// }
+//
+// var indice = cognomi.indexOf(cognome) + 1 ;
+// lista.append('La posizione in cui si trova '+ cognome + ' è: ' + indice);
+// }
+// else{
+//   alert('Devi inserire un cognome,e deve iniziare con la lettera maiuscola');
+// }
+
+//
